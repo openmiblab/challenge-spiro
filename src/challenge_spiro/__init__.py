e8b16eefb56a32ee9886ca1c38fb2e42b8a4267c
Steven Sourbron <s.sourbron@sheffield.ac.uk>
2026-06-27 18:28:53 +0100

    some restructure + bug fix
    Bug in the generalizability score: the settings argument was not passed to the forward function, meaning it applied default data for the experimental conditions. This is fixed

--- src/challenge_spiro/__init__.py
+++ src/challenge_spiro/__init__.py
@@ -1,9 +1,18 @@
-# List all solutions here 
 
-from .solutions.benchmark import inverse as inverse_bench
-from .solutions.normative import inverse as inverse_norm
+# try:    
+#     # Private: Only challenge organisers have access
+#     from .solutions.submitted._registered import REGISTERED
+# except:
+#     REGISTERED = {}
+from .solutions.submitted._registered import REGISTERED
 
-SOLUTIONS = {
+from .solutions.provided.benchmark import inverse as inverse_bench
+from .solutions.provided.benchmark import inverse as inverse_norm
+
+
+
+
+SOLUTIONS = REGISTERED | {
     'Normative': inverse_norm,
     'Benchmark': inverse_bench,
 }
